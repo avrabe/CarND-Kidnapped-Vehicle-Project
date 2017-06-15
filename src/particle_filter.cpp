8d28a5716744af5f47f296dfbdd17e50bc9eea0f
Ralf Anton Beier <ralf_beier@me.com>
2017-06-15 19:52:24 +0200

    First working version.
    Using remote it's running out of time.
    Fixed the problem with the wrong landmark association.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -25,7 +25,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
     //   x, y, theta and their uncertainties from GPS) and all weights to 1.
     // Add random Gaussian noise to each particle.
     // NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-    num_particles = 100;
+    num_particles = 3;
 
     std::normal_distribution<double_t> N_x(x, std[0]);
     std::normal_distribution<double_t> N_y(y, std[1]);
@@ -37,10 +37,10 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
         particle.x = N_x(gen);
         particle.y = N_y(gen);
         particle.theta = N_theta(gen);
-        particle.weight = 1;
+        particle.weight = 1.0;
 
         particles.push_back(particle);
-        weights.push_back(1);
+        weights.push_back(1.0);
     }
 
     is_initialized = true;
@@ -57,17 +57,14 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
         double_t new_x;
         double_t new_y;
         double_t new_theta;
-
-        if (yaw_rate == 0) {
-            new_theta = particle.theta;
-            double_t v_t_d = velocity * delta_t;
-            new_x = particle.x + v_t_d * cos(particle.theta);
-            new_y = particle.y + v_t_d * sin(particle.theta);
-
-        } else {
+        if (yaw_rate != 0) {
+            new_x = particle.x + velocity / yaw_rate * (sin(particle.theta + yaw_rate * delta_t) - sin(particle.theta));
+            new_y = particle.y + velocity / yaw_rate * (cos(particle.theta) - cos(particle.theta + yaw_rate * delta_t));
             new_theta = particle.theta + yaw_rate * delta_t;
-            new_x = particle.x + (velocity / yaw_rate) * (sin(new_theta) - sin(particle.theta));
-            new_y = particle.y + (velocity / yaw_rate) * (cos(particle.theta) - cos(new_theta));
+        } else {
+            new_x = particle.x + velocity * cos(particle.theta) * delta_t;
+            new_y = particle.y + velocity * sin(particle.theta) * delta_t;
+            // particle.theta = particle.theta
         }
 
         std::normal_distribution<double_t> N_x(new_x, std_pos[0]);
@@ -87,6 +84,7 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
     //   implement this method and use it as a helper during the updateWeights phase.
 }
 
+
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
                                    std::vector<LandmarkObs> observations, Map map_landmarks) {
     // TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
@@ -105,48 +103,60 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
         std::vector<int> associations;
         std::vector<double> sense_x;
         std::vector<double> sense_y;
-        double_t weight = 1.0;
+        __long_double_t weight = 1.0;
         w_max = std::numeric_limits<double>::min();
 
         double_t cos_p_t = cos(particle.theta);
         double_t sin_p_t = sin(particle.theta);
-
-        for (auto observation: observations) {
+        for (auto &&observation: observations) {
             // translate the observation into MAP coordinates
+
             double_t tobs_x = particle.x + (observation.x * cos_p_t - observation.y * sin_p_t);
             double_t tobs_y = particle.y + (observation.x * sin_p_t + observation.y * cos_p_t);
+
             // nearest neighbor search
             __long_double_t min_dist = std::numeric_limits<__long_double_t>::max();
             __long_double_t distance = sensor_range;
+            Map::single_landmark_s closest_landmark;
             int association = -1;
-            for (auto landmark : map_landmarks.landmark_list) {
+            for (auto &landmark : map_landmarks.landmark_list) {
                 //distance = sqrt(pow(landmark.x_f - tobs_x, 2.0) + pow(landmark.y_f - tobs_y, 2.0));
-                distance = hypot((tobs_x - landmark.x_f), (tobs_y - landmark.y_f));
-                if (distance < min_dist) {
-                    min_dist = distance;
+
+                double d = dist(landmark.x_f, landmark.y_f, tobs_x, tobs_y);
+                if (d < min_dist && d < sensor_range) {
+                    min_dist = d;
                     association = landmark.id_i;
+                    closest_landmark = landmark;
                 }
+
             }
             if (association >= 0) {
-                double min_x = map_landmarks.landmark_list[association].x_f;
-                double min_y = map_landmarks.landmark_list[association].y_f;
+                assert(closest_landmark.id_i == association);
+                double min_x = closest_landmark.x_f;
+                double min_y = closest_landmark.y_f;
 
-                __long_double_t f = 1.0 / (2 * M_PI * std_landmark[0] * std_landmark[1]);
-                __long_double_t xx = pow(min_x - tobs_x, 2.0) / (2 * pow(std_landmark[0], 2.0));
-                __long_double_t yy = pow(min_y - tobs_y, 2.0) / (2 * pow(std_landmark[1], 2.0));
-                f = f * exp(-1 * (xx + yy));
-                cout << particle.id << " " << f << endl;
-                if (f > 0 && f < std::numeric_limits<__long_double_t>::max()) {
+                __long_double_t f = 2 * M_PI * std_landmark[0] * std_landmark[1];
+                __long_double_t xx = 0.5 * pow(min_x - tobs_x, 2.0) / pow(std_landmark[0], 2.0);
+                __long_double_t yy = 0.5 * pow(min_y - tobs_y, 2.0) / pow(std_landmark[1], 2.0);
+
+                f = exp(-1 * (xx + yy)) / f;
+/*
+                cout << particle.id << " " << f;
+                cout << " mvg(  " << min_x << ", "  << min_y ;
+                cout << " , " << tobs_x << ", " << tobs_y << ")" << endl;
+*/
+                if (f > 0.1e-1000 && f < std::numeric_limits<__long_double_t>::max()) {
                     weight *= f;
                     //assert(weight > 0.0 && weight < 100.0);
                 }
                 associations.push_back(association);
                 sense_x.push_back(tobs_x);
                 sense_y.push_back(tobs_y);
+                weights.push_back(f);
             }
         }
         particle.weight = weight;
-        w_max = (max)(w_max, weight);
+        w_max = (max)(w_max, particle.weight);
         SetAssociations(particle, associations, sense_x, sense_y);
     }
 }
@@ -156,17 +166,19 @@ void ParticleFilter::resample() {
     // NOTE: You may find std::discrete_distribution helpful here.
     //   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
     std::vector<Particle> new_particles;
+/*
     for (auto &&particle: particles) {
         cout << particle.weight << " ";
     }
     cout << endl;
+*/
     int index = 0;
     double_t beta = 0.0;
     std::uniform_real_distribution<double> distribution(0.0, 2 * w_max);
     for (int i = 0; i < num_particles; i++) {
         beta += distribution(gen);
         while (particles[index].weight < beta) {
-            beta = beta - particles[index].weight;
+            beta = beta - (particles[index].weight > 0 ? particles[index].weight : 0.0000001);
             index = (index + 1) % num_particles;
         }
         new_particles.push_back(particles[index]);
@@ -174,7 +186,6 @@ void ParticleFilter::resample() {
     particles = new_particles;
     for (int i = 0; i < num_particles; i++) {
         particles[i].id = i;
-        particles[i].weight = 1.0;
     }
 }
 
---
+++ src/particle_filter_test.cpp
@@ -0,0 +1,4 @@
+//
+// Created by administrator on 6/15/17.
+//
+
